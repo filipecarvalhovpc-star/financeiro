58f2bdb9e3ca97af998e966488df52508519f462
filipecarvalhovpc-star <filipecarvalhovpc@gmail.com>
2026-07-06 02:08:07 -0300

    v6.3 - fonte Inter embutida (visual de app bancario, offline)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C='fin-v6';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./icon-192.png','./icon-512.png','./icon-180.png','./favicon.ico','./manifest.json'])).then(()=>self.skipWaiting()))});
+const C='fin-v7';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./icon-192.png','./icon-512.png','./icon-180.png','./favicon.ico','./manifest.json','./inter-400.woff2?v=6','./inter-500.woff2?v=6','./inter-600.woff2?v=6','./inter-700.woff2?v=6','./inter-800.woff2?v=6'])).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>clients.claim()))});
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
